5b266683843ca141bdbba8aa19824d955bc58143
Shubham Maske <shubham.maske@coradius.io>
2024-07-11 21:52:58 +0530

    joi validation logic added for user registration

--- controllers/auth/registerController.js
+++ controllers/auth/registerController.js
@@ -1,8 +1,23 @@
+import Joi from "joi";
+
+
 const registerController = {
     register(req, res, next){
         //logic
 
+        const resgisterSchema = Joi.object({
+            name: Joi.string().min(2).max(20).required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().pattern(/^[a-zA-Z0-9@]{3,30}$/).required(),
+            repeat_password: Joi.ref('password')
+        })
 
+        const {error} = resgisterSchema.validate(req.body)
+
+        if(error){
+            console.log(error)
+            res.json(error)
+        }
 
         res.json({msg: "From register route"})
     }
